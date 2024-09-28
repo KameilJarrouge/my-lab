43f5a6ebe9527fdb64a315979762cb26be7999b7
Kamil Jarrouj <kamil@localhost.localdomain>
2024-09-28 10:37:21 +0300

    added option to not close modal on outside click, a close button will appear automatically

--- app/_components/Modals/Modal.jsx
+++ app/_components/Modals/Modal.jsx
@@ -1,7 +1,14 @@
 "use client";
 import React, { useEffect } from "react";
+import { MdClose } from "react-icons/md";
 
-function Modal({ isOpen, close, children, uniqueName = "unique" }) {
+function Modal({
+  isOpen,
+  close,
+  children,
+  uniqueName = "unique",
+  shouldCloseOnOutsideClick = true,
+}) {
   const handleOutsideClick = (event) => {
     const content = document.getElementById("content");
     if (!content) {
@@ -13,7 +20,7 @@ function Modal({ isOpen, close, children, uniqueName = "unique" }) {
   };
 
   useEffect(() => {
-    if (!isOpen) return;
+    if (!isOpen || !shouldCloseOnOutsideClick) return;
 
     const modalContainer = document.getElementById(
       `modal-container-${uniqueName}`
@@ -36,8 +43,16 @@ function Modal({ isOpen, close, children, uniqueName = "unique" }) {
         <div className="fixed left-0 top-0 w-full h-full  bg-black bg-opacity-70 z-40 overflow-hidden backdrop-blur flex flex-col justify-center items-center ">
           <div
             id="content"
-            className={`w-fit h-fit bg-dark_primary rounded p-2 animate-popIn shadow-lg shadow-black`}
+            className={`w-fit h-fit bg-dark_primary rounded p-2 animate-popIn shadow-lg shadow-black relative`}
           >
+            {!shouldCloseOnOutsideClick && (
+              <button
+                className="absolute top-[1rem] left-[1rem] z-50 text-text hover:text-white"
+                onClick={close}
+              >
+                <MdClose />
+              </button>
+            )}
             {children}
           </div>
         </div>
