5a5bf6f33ef906a710b063975835254d0c8ef9af
Kamil Jarrouj <kamil@localhost.localdomain>
2024-10-01 16:12:20 +0300

    created routes for urinalysis arbitrary

---
+++ app/api/(user)/arbitrary/urinalysis/update/route.js
@@ -0,0 +1,21 @@
+import { NextRequest } from "next/server";
+import {
+  errorResponse,
+  successResponse,
+} from "../../../../../_lib/responseGenerator";
+import { updateUrinalysisArbitrary } from "@/app/_controllers/arbitraryController";
+export const revalidate = 0;
+
+/**
+ *
+ * @param {NextRequest} request
+ * @returns
+ */
+export async function PUT(request, { params }) {
+  const body = await request.json();
+  let result = await updateUrinalysisArbitrary(body.id, body.urinalysis);
+  if (!result.success) {
+    return errorResponse(result.errorCode);
+  }
+  return successResponse(result.returned);
+}
